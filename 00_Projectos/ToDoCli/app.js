75ab7b56b998ec4526793691d16f41cc345c3de5
Leipsilva9901 <leiliany.1989@gmail.com>
2023-08-25 23:21:29 -0300

    init commit

--- 00_Projectos/ToDoCli/app.js
+++ 00_Projectos/ToDoCli/app.js
@@ -9,8 +9,8 @@ const rl = createInterface({
 });
 
 function displayMenu() {
-  console.log(chalk.yellow.bold("🦊🦊🦊🦊🦊 To Do App 🦊🦊🦊🦊🦊"));
-  console.log(chalk.blueBright("Menu de Opciones:"));
+  console.log(chalk.redBright.bold("🦊🦊🦊🦊🦊 To Do App 🦊🦊🦊🦊🦊"));
+  console.log(chalk.yellow("Menu de Opciones:"));
   console.log("1. Agregar tarea");
   console.log("2. Listar tareas");
   console.log("3. Completar tarea");
@@ -18,80 +18,83 @@ function displayMenu() {
   console.log("\n");
 }
 
-// function addTask() {
-//   rl.question(chalk.bgMagentaBright("Escribe la tarea: "), (task) => {
-//     tasks.push({ task, completed: false });
-//     console.log(chalk.green.bold("Tarea agregada con éxito\n"));
-//     displayMenu();
-//     chooseOption();
-//   });
-// }
+function addTask() {
+  rl.question(chalk.bgMagentaBright("Escribe la tarea: "), (task) => {
+    tasks.push({ task, completed: false });
+    console.log(chalk.green.bold("Tarea agregada con éxito\n"));
+    
+    displayMenu();
+    chooseOption();
+    //console.log(tasks)
+    //console.log(tasks)
+  });
+}
 
-// function listsTasks() {
-//   console.log(chalk.yellow.bold("\n🦊🦊🦊🦊🦊 Tareas 🦊🦊🦊🦊🦊\n"));
+function listsTasks() {
+  console.log(chalk.yellow.bold("\n🦊🦊🦊🦊🦊 Tareas 🦊🦊🦊🦊🦊\n"));
 
-//   if (tasks.length === 0) {
-//     console.log(chalk.green.bold("No hay tareas por hacer 😀👌🏻\n"));
-//   } else {
-//     tasks.forEach((task, index) => {
-//       let status = task.completed ? "✅" : "❌";
+   if (tasks.length === 0) {
+     console.log(chalk.green.bold("No hay tareas por hacer 😀👌🏻\n"));
+   } else {
+     tasks.forEach((task, index) => {
+       let status = task.completed ? "✅" : "❌";
 
-//       if (task.completed) {
-//         console.log(
-//           chalk.greenBright(`${index + 1}. ${status} - ${task.task}`)
-//         );
-//       } else {
-//         console.log(chalk.redBright(`${index + 1}. ${status} - ${task.task}`));
-//       }
-//     });
-//   }
+      if (task.completed) {
+        console.log(
+          chalk.greenBright(`${index + 1}. ${status} - ${task.task}`)
+        );
+      } else {
+         console.log(chalk.redBright(`${index + 1}. ${status} - ${task.task}`));
+       }
+     });
+   }
 
-//   displayMenu();
-//   chooseOption();
-// }
+  displayMenu();
+   chooseOption();
+ }
 
-// function completeTask() {
-//   rl.question(
-//     chalk.bgMagentaBright("Digita el número de la tarea a completar: "),
-//     (taskNumber) => {
-//       const index = parseInt(taskNumber) - 1;
-//       if (index >= 0 && index < tasks.length) {
-//         tasks[index].completed = true;
-//         console.log(chalk.green.bold("Tarea marcada con exito ✅\n"));
-//       } else {
-//         console.log(chalk.red.bold("Número de tarea inválido \n"));
-//       }
-//       displayMenu();
-//       chooseOption();
-//     }
-//   );
-// }
+function completeTask() {
+  rl.question(
+    chalk.bgMagentaBright("Digita el número de la tarea a completar: "),
+    (taskNumber) => {
+      const index = parseInt(taskNumber) - 1;
+      if (index >= 0 && index < tasks.length) {
+        tasks[index].completed = true;
+        console.log(chalk.green.bold("Tarea marcada con exito ✅\n"));
+      } else {
+        console.log(chalk.red.bold("Número de tarea inválido \n"));
+      }
+      displayMenu();
+      chooseOption();
+    }
+   );
+}
 
-// function chooseOption() {
-//   rl.question("Digita el número de tu opción:", (choice) => {
-//     switch (choice) {
-//       case "1":
-//         addTask();
-//         break;
-//       case "2":
-//         listsTasks();
-//         break;
-//       case "3":
-//         completeTask();
-//         break;
-//       case "4":
-//         4;
-//         console.log(chalk.yellow("Adiós 👋🏻🦊"));
-//         rl.close();
-//         break;
-//       default:
-//         console.log(chalk.red("Opción Inválida, Intenta nuevamente \n"));
-//         displayMenu();
-//         chooseOption();
-//         break;
-//     }
-//   });
-// }
+function chooseOption() {
+  rl.question("Digita el número de tu opción:", (choice) => {
+    switch (choice) {
+      case "1":
+        addTask();
+        break;
+      case "2":
+        listsTasks();
+        break;
+      case "3":
+        completeTask();
+        break;
+      case "4":
+        4;
+        console.log(chalk.yellow("Adiós 👋🏻🦊"));
+        rl.close();
+        break;
+      default:
+        console.log(chalk.red("Opción Inválida, Intenta nuevamente \n"));
+        displayMenu();
+        chooseOption();
+        break;
+     }
+  });
+}
 
 displayMenu();
-//chooseOption();
+chooseOption();
